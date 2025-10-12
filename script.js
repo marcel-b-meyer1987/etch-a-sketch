905d1f79f44922a1f3623b15a9a326377383771e
Marcel Meyer <23430036+marcel-b-meyer1987@users.noreply.github.com>
2025-10-12 21:59:25 +0200

    add further styling and algorithm to calculate square size

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init() {
 
-    const container = document.querySelector("#container");
+    var container = document.querySelector("#container");
     console.log(container);
 
     const startBtn = document.querySelector("#reset-Btn");
@@ -18,19 +18,31 @@ function createGrid() {
     event.preventDefault();
 
     const defaultCount = 16;
+    let count = defaultCount;
 
     console.log("createGrid()...");
     console.log(`defaultCount: ${defaultCount}`);
 
     let countStr = prompt("Please enter a number of squares per row:", "16");
-    /* if (countStr != null && countStr != "") {
-        let count = parseInt(countStr) ?? defaultCount;
+    if ((countStr != null) && (countStr != "") && (! isNaN(parseInt(countStr)))) {
+        count = parseInt(countStr) ?? defaultCount;
     }
-    else {
-        let count = defaultCount;
-    } */
-    let count = parseInt(countStr);
+    
     console.log(`countStr: ${countStr} => count: ${count}`);
+
+    const containerWidth = Math.floor(visualViewport.height * 70 / 100);
+    const containerHeight = containerWidth;
+    console.log(`Grid dimensions: ${containerWidth} x ${containerHeight}px`);
+
+    const squareWidth = Math.floor((containerWidth - (count - 1)) / count);
+    const squareHeight = squareWidth;
+    console.log(`Square dimensions: ${squareWidth} x ${squareHeight}px`);
+/* 
+    container.styles.width = containerWidth;
+    container.styles.height = containerHeight; */
+
+
+
     
 }
 
