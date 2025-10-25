57e7e809da085ec74ff15a40ef2956dbf4cd6672
Marcel Meyer <23430036+marcel-b-meyer1987@users.noreply.github.com>
2025-10-25 12:13:04 +0200

    fix styles and add feature to choose number of squares

--- script.js
+++ script.js
@@ -13,18 +13,12 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(resetBtn);
     console.log(container);
 
-
-
     resetBtn.addEventListener("click", createSquares)
-
-
-
 
 });
 
 function createSquares() {
 
-    // squaresCount = prompt("Please enter the wanted number of squares per row.", "24");
     const container = document.querySelector("#container");    
 
     if (container.hasChildNodes) {
@@ -32,49 +26,36 @@ function createSquares() {
         squares.splice(0, squares.length);
     }
     
+    const mode = document.querySelector("#mode");
+    console.log(mode.value);
 
-/*     // First calculate the dimensions of the squares, based on actual container size + wanted number of squares
-    let cWidth = container.offsetWidth;
-    let borderSpace = squaresCount - 1;
-    let squareWidth = Math.floor((cWidth - borderSpace) / squaresCount);
-    console.log("Calculating dimensions of squares...");
-    console.log(`Container Width: ${cWidth}px`);
-    console.log(`Space needed for ${(squaresCount - 1)} borders: ${borderSpace}px`);
-    console.log(`Available width/height per square: ${squareWidth}px`);
-
-    for (let i = 0; i < squaresCount * squaresCount; i++) {
-
-        const newSquare = document.createElement("div");
-        newSquare.setAttribute("width", `${squareWidth}px`);
-        newSquare.setAttribute("height", `${squareWidth}px`);
-        // newSquare.setAttribute("flex-grow","0");
-        container.appendChild(newSquare);
-        squares.push(newSquare);
-
-    } */
-
-        const mode = document.querySelector("#mode");
-        console.log(mode.value);
+    squaresCount = prompt("Please enter the wanted number of squares per row.", "24");
     
-    for (let i = 0; i < (squaresCount * squaresCount); i++) {
+    for (let i = 0; i < squaresCount; i++) {
 
-        const newSquare = document.createElement("div");
+        for (let j = 0; j < squaresCount; j++) {
 
-        switch(mode.value) {
+            const newSquare = document.createElement("div");
 
-            case "Darken": 
-                newSquare.style.opacity = "0.1";
-                newSquare.addEventListener("mouseover", darkenSquare);
-                break;
+            newSquare.style.flex = `1 1 calc(100% / ${squaresCount})`;
 
-            case "Blacken": 
-                newSquare.addEventListener("mouseover", blackenSquare);
-                break;
+            switch(mode.value) {
 
-        }
+                case "Darken": 
+                    newSquare.style.opacity = "0.1";
+                    newSquare.addEventListener("mouseover", darkenSquare);
+                    break;
 
-        squares.push(newSquare);
-        container.appendChild(newSquare);
+                case "Blacken": 
+                    newSquare.addEventListener("mouseover", blackenSquare);
+                    break;
+
+            }
+
+            squares.push(newSquare);
+            container.appendChild(newSquare);
+
+        }        
 
     }
     
